e16e963056ce88d19ffacf7cc00eea666aa55f9b
Aristondo01 <sebastianaristondo@gmail.com>
2022-05-23 07:53:47 -0600

    Diseño cuarto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Marco from './components/Marco.jsx'
+
+function App() {
+
+  return (
+    <div className='fondo'>
+      <Marco/>
+    </div>
+  )
+}
+
+export default App
