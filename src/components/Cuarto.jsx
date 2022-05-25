d971fa40bb7a219f2240615831fd2afcc2ef5825
Aristondo01 <sebastianaristondo@gmail.com>
2022-05-24 21:11:41 -0600

    Seleccionar libro

--- src/components/Cuarto.jsx
+++ src/components/Cuarto.jsx
@@ -6,16 +6,18 @@ import Adornos from './Adornos.jsx'
 
 const Cuarto = () => {
     const [encendido, setEncendido] = useState(false)
-    const prender = () => setEncendido(!encendido)
-    const [libro, setLibro] = useState(' ')
-
+    const [libro, setLibro] = useState('Nothing in reproduction')
+    const prender = () => {
+        setEncendido(!encendido)
+        setLibro('Nothing in reproduction')
+    }
     return (
-        <div className="vista" style={encendido ? { opacity: 0.2 } : { opacity: 1 }}>
+        <div className="vista" style={encendido ? { opacity: 1 } : { opacity: 0.2 }}>
             <div className="pared">
                 <Adornos />
-                <button type="button" className="flipon" onClick={prender}> </button>
+                <button type="button" className="flipon" onClick={prender} style={encendido ? { } : { animation: 'latidos .5s infinite' }}> </button>
                 <Television libro={libro} />
-                <Librera setLibro={setLibro} />
+                <Librera encendido={encendido} libro={libro} setLibro={setLibro} />
             </div>
             <div className="piso" />
 
--- src/components/Librera.jsx
+++ src/components/Librera.jsx
@@ -1,21 +1,37 @@
+import { useEffect, useState } from 'react'
 import './Cuarto.css'
 import './Librera.css'
+import PropTypes from 'prop-types'
 
-const Librera = () => (
+const Librera = ({ encendido, libro, setLibro }) => {
+    const [contenido, setContenido] = useState(true)
 
-    <div className="librera">
-        <div className="CajonArriba">
-            <div className="Platino" />
-            <div className="Estrella" />
+    useEffect(() => {
+        if (encendido && libro === 'Nothing in reproduction') setContenido(true)
+        else setContenido(false)
+    }, [encendido, libro])
+
+    const ContentHandler = (valor) => setLibro(valor)
+    return (
+        <div className="librera">
+            <div className="CajonArriba">
+                <div className="Platino" />
+                <div className="Estrella" />
+            </div>
+            <div className="CajonCentro" style={contenido ? { animation: 'latidos2 .5s infinite' } : { }}>
+                <button type="button" className="work" onClick={() => ContentHandler('Work')}> </button>
+                <button type="button" className="figma" onClick={() => ContentHandler('Figma')}> </button>
+                <button type="button" className="replicas" onClick={() => ContentHandler('Replicas')}> </button>
+                <button type="button" className="skills" onClick={() => ContentHandler('Skills')}> </button>
+            </div>
         </div>
-        <div className="CajonCentro">
-            <div className="libroc" />
-            <div className="librod" />
-            <div className="libroc" />
-            <div className="librod" />
+    )
+}
 
-        </div>
-    </div>
-)
+Librera.propTypes = {
+    encendido: PropTypes.bool.isRequired,
+    libro: PropTypes.string.isRequired,
+    setLibro: PropTypes.func.isRequired,
+}
 
 export default Librera
