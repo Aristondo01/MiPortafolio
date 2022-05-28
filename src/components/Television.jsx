ef44c07654ec6076451163a46104d99efb803984
Aristondo01 <sebastianaristondo@gmail.com>
2022-05-28 11:13:21 -0600

    Arreglo rutas

--- src/components/Television.jsx
+++ src/components/Television.jsx
@@ -1,12 +1,13 @@
 import './Cuarto.css'
 import './Television.css'
 import PropTypes from 'prop-types'
+import Replicas from '../../public/Replicas.json'
 
 const Television = ({ libro }) => (
 
     <div className="centro">
         <div className="tele">
-            <div className="pantalla">{libro}</div>
+            <div className="pantalla" style={{ backgroundImage: `url(${Replicas[0].miniatura})` }}>{libro}</div>
         </div>
         <div className="mueble" />
     </div>
