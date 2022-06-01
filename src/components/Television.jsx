08bf163484faacaf899466c90fac1e30c6697862
Aristondo01 <sebastianaristondo@gmail.com>
2022-06-01 17:01:22 -0600

    Creacion de todos los JSON

--- src/components/Television.jsx
+++ src/components/Television.jsx
@@ -6,34 +6,38 @@ import './Television.css'
 import PropTypes from 'prop-types'
 import Replicas from '../../public/Replicas.json'
 import Work from '../../public/Work.json'
+import Skills from '../../public/Skills.json'
+import Figma from '../../public/Figma.json'
 
 const Television = ({ libro }) => {
     const [pantalla, setPantalla] = useState()
     const viendo = useRef(Work)
     const [vacio, setVacio] = useState(false)
     const interval = useRef(null)
-    const contador = useRef(1)
+    const contador = useRef(0)
     const Intervalo = (() => {
         clearInterval(interval.current)
         setPantalla(viendo.current[contador.current].miniatura)
         interval.current = setInterval(() => {
-            contador.current = (contador.current + 1) % 2
+            contador.current = (contador.current + 1) % viendo.current.length
             setPantalla(viendo.current[contador.current].miniatura)
-        }, 6000)
+        }, 3500)
     })
 
     useEffect(() => {
+        contador.current = 0
         if (libro === 'Work') {
             viendo.current = Work
             Intervalo()
         } else if (libro === 'Figma') {
-            viendo.current = Work
+            viendo.current = Figma
             Intervalo()
         } else if (libro === 'Replicas') {
             viendo.current = Replicas
             Intervalo()
-        } else {
-            viendo.current = []
+        } else if (libro === 'Skills') {
+            viendo.current = Skills
+            Intervalo()
         }
         if (libro !== 'Nothing in reproduction') {
             setVacio(true)
@@ -46,7 +50,7 @@ const Television = ({ libro }) => {
             <div className="tele">
                 { vacio
                     ? <div className="pantalla" style={{ backgroundImage: `url(${pantalla})`, backgroundColor: '#aba9a4' }} />
-                    : <div className="pantalla" style={{ backgroundColor: 'black' }} />}
+                    : <div className="pantalla" />}
             </div>
             <div className="mueble" />
         </div>
